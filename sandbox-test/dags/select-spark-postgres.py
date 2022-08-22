f011f30a0bd07b488d1c2ffc2fb1a736b2848b26
paolodeguisse <84400108+paolodeguisse@users.noreply.github.com>
2022-08-22 11:05:08 +0300

    Create select-spark-postgres.py

---
+++ sandbox-test/dags/select-spark-postgres.py
@@ -0,0 +1,51 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+from datetime import datetime, timedelta
+
+###############################################
+# Parameters
+###############################################
+spark_master = "spark://spark:7077"
+postgres_driver_jar = "/usr/local/spark/resources/postgresql-42.4.2.jar"
+
+postgres_db = "jdbc:postgresql://151.248.122.146:5432/airflow"
+
+now = datetime.now()
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(now.year, now.month, now.day),
+    "email": ["airflow@airflow.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=1)
+}
+
+dag = DAG(
+        dag_id="select", 
+        description="This DAG reads postgres",
+        default_args=default_args, 
+        schedule_interval=timedelta(1)
+    )
+
+start = DummyOperator(task_id="start", dag=dag)
+
+
+spark_job_read_postgres = SparkSubmitOperator(
+    task_id="spark_job_select",
+    application="/usr/local/spark/app/select.py", # Spark application path created in airflow and spark cluster
+    name="select-postgres",
+    conn_id="spark_default",
+    verbose=1,
+    conf={"spark.master":spark_master},
+    application_args=[postgres_db],
+    jars=postgres_driver_jar,
+    driver_class_path=postgres_driver_jar,
+    dag=dag)
+
+end = DummyOperator(task_id="end", dag=dag)
+
+start >> spark_job_load_postgres >> spark_job_read_postgres >> end
